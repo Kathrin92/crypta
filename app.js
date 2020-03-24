0a30612b62f0ac01fe55965948c397daa745d73f
Kathrin92 <kathrinulmer@gmx.de>
2020-03-24 11:37:39 +0100

    Add question to ask for password

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const { get, set, unset } = require("./lib/commands");
-
-const [command, key, value] = process.argv.slice(2);
-
-if (command === "get") {
-  get(key);
-} else if (command === "set") {
-  set(key, value);
-} else if (command === "unset") {
-  unset(key);
-} else {
-  console.error("Unknown command");
+const [command, key] = process.argv.slice(2);
+const { askPassword } = require("./lib/questions");
+async function run() {
+  if (command === "get") {
+    get(key);
+  } else if (command === "set") {
+    const password = await askPassword();
+    set(key, password);
+  } else if (command === "unset") {
+    unset(key);
+  } else {
+    console.error("Unknown command");
+  }
 }
+run();
